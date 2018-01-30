3faac41e4373c7fe2893bac813ff7dd810ffed39
David Trinh <d.trinh@ohwee.fr>
2018-01-30 17:01:08 +0100

    poc

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use A2lix\I18nDoctrineBundle\Annotation\I18nDoctrine;
 use App\Entity\BlogPost;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,11 +13,13 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/{_locale}", name="default", defaults={"_locale": "fr"})
+     * @I18nDoctrine()
      */
     public function index(Request $request)
     {
         $posts = $this->getDoctrine()->getRepository(BlogPost::class)->findAll();
+        var_dump($request->getLocale());
         return $this->render('base.html.twig', ['posts' => $posts]);
     }
 }
